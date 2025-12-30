5464dda83dd6c56cc4e1909fa691d90cebb66c5d
Peiko Family <peiko.oleh@gmail.com>
2025-12-30 04:06:42 +0100

    Delete api directory

--- api/ping.js
+++
@@ -1,8 +0,0 @@
-// api/ping.js
-export default function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    message: 'Backend for landing is alive ✨',
-    time: new Date().toISOString()
-  });
-}
